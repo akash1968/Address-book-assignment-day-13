33355f68847ebc2e964913c90a572f004c22a78d
Akash <akash.singh670@gmail.com>
2020-10-19 01:20:50 +0530

    [Akash] .Added UC10_Ability to get number of contact persons i.e count by City or State in Address book assignment for day 13

--- AddressBook_Day_13/AddressBook.cs
+++ AddressBook_Day_13/AddressBook.cs
@@ -18,9 +18,9 @@ namespace AddressBook_Day_13
         //create a dictionary(generic collection) to store keyvalue pair
         private Dictionary<string, Contact> d = new Dictionary<string, Contact>();
         //create a city dictionary to store city details
-        private Dictionary<string, Contact> cityDictionary = new Dictionary<string, Contact>();
+        public static Dictionary<string, Contact> cityDictionary = new Dictionary<string, Contact>();
         //create a state dictionary to store state details
-        private Dictionary<string, Contact> stateDictionary = new Dictionary<string, Contact>();
+        public static Dictionary<string, Contact> stateDictionary = new Dictionary<string, Contact>();
         public List<Contact> GetList()
         {
             return list;
@@ -192,6 +192,32 @@ namespace AddressBook_Day_13
                 Console.WriteLine();
             }
 
+        }
+        //UC10-Count by city name
+        public  void GetCountByCity()
+        {
+            Console.WriteLine("Enter a city name to get number of contacts within the location:");
+            string cityName = Console.ReadLine().ToUpper();
+            Console.WriteLine("\nNumber of contact persons present at {0}:", cityName);
+            /// CHECKS IF THE DICTIONARY ALREADY CONTAINS THE CITY/STATE
+            if (cityDictionary.ContainsKey(cityName))
+                ///DISPLAYS THE COUNT
+                Console.WriteLine(cityDictionary[cityName].Count);
+            else
+                Console.WriteLine(0);
+        }
+        //UC10-Count by state name
+        public void GetCountByState()
+        {
+            Console.WriteLine("Enter state name to get number of contacts within the location:");
+            string StateName = Console.ReadLine().ToUpper();
+            Console.WriteLine("\nNumber of contact persons present at {0}:", StateName);
+            /// CHECKS IF THE DICTIONARY ALREADY CONTAINS THE CITY/STATE
+            if (stateDictionary.ContainsKey(StateName))
+                ///DISPLAYS THE COUNT
+                Console.WriteLine(stateDictionary[StateName].Count);
+            else
+                Console.WriteLine(0);
         }
     }
 }
